4beed0317dc47dee700cb486de9217de12b75061
Azzuhry <muhammadazzuhry03@gmail.com>
2020-12-18 23:19:14 +0700

    Appbar components has finished

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-import main from './src/script/view/main.js';
+import "./src/script/component/AppBar.js";
+import main from "./src/script/view/main.js";
+
 document.addEventListener("DOMContentLoaded", main);
