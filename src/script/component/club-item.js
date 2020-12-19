868668ab7ea0feb7030880e381b9273d9490fee4
Azzuhry <muhammadazzuhry03@gmail.com>
2020-12-19 23:17:54 +0700

    fixed previous problems

--- src/script/component/club-item.js
+++ src/script/component/club-item.js
@@ -1,17 +1,17 @@
-class ClubItem extend HTMLElement {
+class ClubItem extends HTMLElement {
     set club(club) {
         this._club = club;
         this.render();
     }
 
     render() {
-        clubElement.innerHTML = `
-        <img class="fan-art-club" src="  ${fanArt}  " alt="Fan Art">
-        <div class="club-info">
-            <h2>${this._club.name}</h2>
-            <p>${this._club.description}</p> 
-        </div>`;
+        this.innerHTML = `
+           <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
+           <div class="club-info">
+               <h2>${this._club.name}</h2>
+               <p>${this._club.description}</p>
+           </div>`;
     }
 }
 
-customElements("club-item", ClubItem);
+customElements.define("club-item", ClubItem);
